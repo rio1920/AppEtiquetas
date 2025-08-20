e089c606c26cf1bc58027ce8e20a3aef7652664a
Juan Salerno <jpsalerno91gmail.com>
2025-08-20 12:08:31 -0300

    nuevo testss

--- etiquetas/tests/tests_models.py
+++ etiquetas/tests/tests_models.py
@@ -4,4 +4,4 @@ from etiquetas.models import Impresora
 @pytest.mark.django_db
 def test_tipo_impresoras():
     impresora = Impresora.objects.create(dpi=300, descripcion="Impresora de etiquetas")
-    assert str(impresora) == "(300 DPIS)"
+    assert str(impresora) == "(300 DPI)"
